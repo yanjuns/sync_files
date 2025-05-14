25bef1b7ae70c474499b23f816af63ba6f055356
Yanjun Sun <yartsun@gmail.com>
2025-05-14 16:44:31 -0700

    update on May 14, 2025

--- start_sync.m
+++ start_sync.m
@@ -1,11 +1,11 @@
 %% BIDIRECTIONAL SYNC FILES
 %sync files in both folder, resulting in an identical two folders
-folderA = 'C:\Users\Yanjun Sun\Desktop\K01';
-folderB = 'C:\Users\Yanjun Sun\OneDrive - Stanford\Writing_grants\K01';
+folderA = 'C:\Users\Yanjun Sun\Desktop\Writing_grants\Simons';
+folderB = 'C:\Users\Yanjun Sun\OneDrive - Stanford\Writing_grants\SCGB_TTI_2024';
 bidirectional_sync_files(folderA, folderB);
 
 %% ONE DIRECTIONAL SYNC FILES
-%only copy files in one folder to another folder
+%only copy files in one folder to another folder (from A to B)
 folderA = 'C:\Users\Yanjun Sun\Desktop\FolderA';
 folderB = 'C:\Users\Yanjun Sun\Desktop\FolderB';
 onedirectional_sync_files(folderA, folderB);
